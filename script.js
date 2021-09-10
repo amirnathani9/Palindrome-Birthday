6b231c65de97a5025385aa43fcdd9057e53e7bd2
Aamir Nathani <amirnathani9@gmail.com>
2021-09-10 16:03:42 +0530

    added: convertToString funct

--- script.js
+++ script.js
@@ -10,6 +10,34 @@ function isPalindrome(str){
     return str === reversedString;
 }
 
-console.log(isPalindrome('oppo'))
+
+function getDateAsString(date){
+
+    var dateStr = {day: '', month: '', year: ''};
+
+    if(date.day < 10){
+        dateStr.day = '0' + date.day;
+    }
+    else{
+        dateStr.day = date.day.toString()
+    }
+    if(date.month < 10){
+        dateStr.month = '0' + date.month;
+    }
+    else{
+        dateStr.month = date.month.toString()
+    }
+
+    dateStr.year = date.year.toString();
+
+    return dateStr
+}
+
+
+var date = {day: 4, month: 05, year: 2020};
+
+console.log(getDateAsString(date))
+
+
 
 
