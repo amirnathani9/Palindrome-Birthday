a58dfd0882fc0a508d041a70ba062605dd505d39
Aamir Nathani <amirnathani9@gmail.com>
2021-09-12 18:18:41 +0530

    added: next date function

--- script.js
+++ script.js
@@ -1,30 +1,32 @@
-function reverseString(str){
+function reverseString(str) {
     var listOfChars = str.split('');
     var listOFReverseChar = listOfChars.reverse();
     var reversedString = listOFReverseChar.join('');
     return reversedString;
 }
 
-function isPalindrome(str){
+function isPalindrome(str) {
     var reversedString = reverseString(str);
     return str === reversedString;
 }
 
 
-function getDateAsString(date){
+function getDateAsString(date) {
 
-    var dateStr = {day: '', month: '', year: ''};
+    var dateStr = {
+        day: '',
+        month: '',
+        year: ''
+    };
 
-    if(date.day < 10){
+    if (date.day < 10) {
         dateStr.day = '0' + date.day;
-    }
-    else{
+    } else {
         dateStr.day = date.day.toString()
     }
-    if(date.month < 10){
+    if (date.month < 10) {
         dateStr.month = '0' + date.month;
-    }
-    else{
+    } else {
         dateStr.month = date.month.toString()
     }
 
@@ -33,7 +35,7 @@ function getDateAsString(date){
     return dateStr
 }
 
-function getDateInAllFormats(date){
+function getDateInAllFormats(date) {
 
     var ddmmyyyy = date.day + date.month + date.year;
     var mmddyyyy = date.month + date.day + date.year;
@@ -45,25 +47,74 @@ function getDateInAllFormats(date){
     return [ddmmyyyy, mmddyyyy, yyyymmdd, ddmmyy, mmddyy, yymmdd]
 }
 
-function checkPalindromeForAllFormats(date){
+function checkPalindromeForAllFormats(date) {
     var listOfDateFormat = getDateInAllFormats(date);
     var palindromeList = [];
-    for(var i =0 ; i<listOfDateFormat.length; i++){
+    for (var i = 0; i < listOfDateFormat.length; i++) {
         var result = isPalindrome(listOfDateFormat[i])
         palindromeList.push(result);
     }
     return palindromeList;
 }
 
+function leapYear(year) {
+    if (year % 400 === 0)
+        return true;
 
-var date = {day: 02, month: 2, year: 2020};
-var dateStr = getDateAsString(date);
+    if (year % 100 === 0)
+        return false;
 
-console.log(getDateInAllFormats(dateStr));
+    if (year % 4 === 0)
+        return true;
 
-console.log(checkPalindromeForAllFormats(dateStr))
+    return false;
+}
+
+function getNextDate(date) {
+    var day = date.day + 1;
+    var month = date.month;
+    var year = date.year;
+
+    var daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+
+    if (month === 2) {
+        if (leapYear(year)) {
+            if (day > 29) {
+                day = 1;
+                month++
+            }
+        } else {
+            if (day > 28) {
+                day = 1;
+                month++;
+            }
+        }
+    } else {
+        if (day > daysInMonth[month - 1]) {
+            day = 1;
+            month++;
+        }
+    }
+
+    if (month > 12) {
+        month = 1;
+        year++;
+    }
+    return {
+        day: day,
+        month: month,
+        year: year,
+    };
+}
 
 
 
+var date = {
+    day: 31,
+    month: 12,
+    year: 2020
+};
 
 
+console.log(getNextDate(date));
